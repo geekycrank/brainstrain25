87e43f6b9fd6633158a99910eaae8a2a01ebcbc3
Maharaja <m.maharaja1964@gmail.com>
2025-10-02 17:44:10 +0530

    Add main page and styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Register from "./components/Register";
+import MainPage from "./components/MainPage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
-      
-      <Register/>
-  
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </Router>
   );
 }
